1c20d481e2b938f35bd2098b7c2d7d8569ec407a
Katherine Watkins <watkins.katherine@student.greenriver.edu>
2023-01-25 21:35:07 -0500

    added initial project files: .htaccess, .gitignore, composer.json, and index.php

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+//Create an instance of the base class of fat-free
+$f3 = Base::instance();
+
+
+//define default route -> home.html
+$f3->route('GET /', function (){
+
+$view = new Template();
+echo $view-> render('views/home.html');
+});
+
+//define "About" page route
+$f3->route('GET /resources', function (){
+
+$view = new Template();
+echo $view-> render('views/about.html');
+});
+
+//define "Available Pets" page route
+$f3->route('GET /availablepets', function (){
+
+$view = new Template();
+echo $view-> render('views/availablepets.html');
+});
+
+//define "Found Pets" page route
+$f3->route('GET /foundpets', function (){
+
+    $view = new Template();
+    echo $view-> render('views/foundpets.html');
+});
+
+//define "Resources" page route
+$f3->route('GET /resources', function (){
+
+    $view = new Template();
+    echo $view-> render('views/resources.html');
+});
+
+
+//application form route
+$f3->route('GET|POST /apply', function ($f3){
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+$_SESSION['name'] = $_POST['name'];
+$_SESSION['age'] = $_POST['age'];
+
+$f3->reroute('summary');
+}
+
+$view = new Template();
+echo $view-> render('views/apply.html');
+});
+
+//order form route
+$f3->route('GET /summary', function (){
+$view = new Template();
+echo $view-> render('views/summary.html');
+});
+
+//run instance of fat-free
+$f3->run();
