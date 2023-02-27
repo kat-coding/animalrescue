a8c07072c7f685b52644c20f36959433679ad350
Dee Brecke <brecke.deirdre@student.greenriver.edu>
2023-02-27 11:32:33 -0500

    Added child classes with getters and setters

---
+++ classes/lostpet.php
@@ -0,0 +1,57 @@
+<?php
+class LostPet extends Pets{
+    private $_ownerName;
+    private $_email;
+    private $_phone;
+    private $_dateMissing;
+
+    function __construct($ownerName, $email, $phone, $dateMissing){
+        parent::__construct();
+        $this->_ownerName = $ownerName;
+        $this->_email = $email;
+        $this->_phone = $phone;
+        $this->_dateMissing = $dateMissing;
+    }
+
+    function getOwnerName()
+    {
+        return $this->_ownerName;
+    }
+
+    function setOwnerName($ownerName)
+    {
+        $this->_ownerName = $ownerName;
+    }
+
+    function getEmail()
+    {
+        return $this->_email;
+    }
+
+    function setEmail($email)
+    {
+        $this->_email = $email;
+    }
+
+    function getPhone()
+    {
+        return $this->_phone;
+    }
+
+    function setPhone($phone)
+    {
+        $this->_phone = $phone;
+    }
+
+    function getDateMissing()
+    {
+        return $this->_dateMissing;
+    }
+
+    function setDateMissing($dateMissing)
+    {
+        $this->_dateMissing = $dateMissing;
+    }
+
+
+}//end of class
---
+++ classes/shelterpet.php
@@ -0,0 +1,55 @@
+<?php
+class ShelterPet extends Pets{
+    private $_status;
+    private $_goodWithKids;
+    private $_goodWithOtherPets;
+    private $_healthConditions;
+
+    function __construct($status, $goodWithKids, $goodWithOtherPets, $healthConditions){
+        parent::__construct();
+        $this->_status = $status;
+        $this->_goodWithKids = $goodWithKids;
+        $this->_goodWithOtherPets = $goodWithOtherPets;
+        $this->_healthConditions = $healthConditions;
+    }
+
+    function getStatus()
+    {
+        return $this->_status;
+    }
+
+    function setStatus($status)
+    {
+        $this->_status = $status;
+    }
+
+    function getGoodWithKids()
+    {
+        return $this->_goodWithKids;
+    }
+
+    function setGoodWithKids($goodWithKids)
+    {
+        $this->_goodWithKids = $goodWithKids;
+    }
+
+    function getGoodWithOtherPets()
+    {
+        return $this->_goodWithOtherPets;
+    }
+    function setGoodWithOtherPets($goodWithOtherPets)
+    {
+        $this->_goodWithOtherPets = $goodWithOtherPets;
+    }
+    function getHealthConditions()
+    {
+        return $this->_healthConditions;
+    }
+
+    function setHealthConditions($healthConditions)
+    {
+        $this->_healthConditions = $healthConditions;
+    }
+
+
+}//end of class
