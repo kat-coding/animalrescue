e06b842b9650b35221692e084be2b9099d3bf3ae
Katherine Watkins <watkins.katherine@student.greenriver.edu>
2023-03-14 19:45:04 -0400

    put back validation code that was accidentally deleted

---
+++ model/validate.js
@@ -0,0 +1,102 @@
+$("#shelterform").on("submit", function (){
+
+    let name = $("#name").val()
+
+    if(name == ""){
+
+        $("#err-name").css("display", "block")
+
+        return false;
+
+    }
+
+    let age = $("#age").val()
+
+    if(age == ""){
+
+        $("#err-age").css("display", "block")
+
+        return false;
+
+    }
+
+    let species = $("#species").val()
+
+    if(species == ""){
+
+        $("#err-species").css("display", "block")
+
+        return false;
+
+    }
+
+    let sex = $("#sex").val()
+
+    if(sex == ""){
+
+        $("#err-sex").css("display", "block")
+
+        return false;
+
+    }
+
+    let city = $("#city").val()
+
+    if(city == ""){
+
+        $("#err-city").css("display", "block")
+
+        return false;
+
+    }
+
+    let state = $("#state").val()
+
+    if(state == ""){
+
+        $("#err-state").css("display", "block")
+
+        return false;
+
+    }
+
+    let status = $("#status").val()
+
+    if(status == ""){
+
+        $("#err-status").css("display", "block")
+
+        return false;
+
+    }
+
+    let kids = $("#kids").val()
+
+    if(kids == ""){
+
+        $("#err-kids").css("display", "block")
+
+        return false;
+
+    }
+
+    let pets = $("#pets").val()
+
+    if(pets == ""){
+
+        $("#err-pets").css("display", "block")
+
+        return false;
+
+    }
+
+    let health = $("#health").val()
+
+    if(health == ""){
+
+        $("#err-health").css("display", "block")
+
+        return false;
+
+    }
+})
